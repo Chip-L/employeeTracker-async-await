c15f336d794943ba308ba84338db152c41c06216
Chip Long <chip_long@yahoo.com>
2021-05-21 23:55:21 -0600

    saved changes to the  set up mySQL DB

--- employeeDB.sql
+++ employeeDB.sql
@@ -7,15 +7,15 @@ CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT FOREIGN KEY,
-  manager_id INT FOREIGN KEY
+  role_id INT,
+  manager_id INT
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30), 
-  salary DECIMAL(6,2),
-  department_id INT FOREIGN KEY
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(6 , 2 ),
+    department_id INT
 );
 
 CREATE TABLE department (
