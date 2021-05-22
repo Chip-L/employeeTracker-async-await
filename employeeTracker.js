2a00fba22d3281be0b33777d8144a9c19e7d81b0
Chip Long <chip_long@yahoo.com>
2021-05-22 03:09:51 -0600

    added starting prompt

--- employeeTracker.js
+++ employeeTracker.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   database: "employeeDB",
 });
 
-function showStartScreen() {
+const showStartScreen = () => {
   console.log(",---------------------------------------------------.");
   console.log("|   _____                 _                         |");
   console.log("|  | ____|_ __ ___  _ __ | | ___  _   _  ___  ___   |");
@@ -27,12 +27,57 @@ function showStartScreen() {
   console.log("|                            |___/                  |");
   console.log("|                                                   |");
   console.log("`---------------------------------------------------'");
-}
+};
+
 // get options
 function start() {
-  showStartScreen();
-
-  connection.end();
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Exit program",
+        ],
+        name: "choice",
+      },
+    ])
+    .then((answer) => {
+      switch (answer.choice) {
+        case "View All Employees":
+          break;
+        case "View All Employees By Department":
+          break;
+        case "View All Employees By Manager":
+          break;
+        case "Add Employee":
+          break;
+        case "Remove Employee":
+          break;
+        case "Update Employee Role":
+          break;
+        case "Update Employee Manager":
+          break;
+        default:
+          connection.end();
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        throw new Error(
+          "Prompt couldn't be rendered in the current environment."
+        );
+      } else {
+        throw error;
+      }
+    });
 }
 
 // add department
@@ -66,5 +111,7 @@ function start() {
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as is ${connection.threadId}\n`);
+
+  showStartScreen();
   start();
 });
